8e5681059dccb0b4020c4a6404a340050152cc41
Oleg Larkin <krabique48@gmail.com>
2017-07-02 23:53:23 +0000

    Add mobile support and Add cancancan authorization in views for certain actions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,11 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   
   def index
-    @users = User.all
     @movies = Movie.all
     @movie_sessions = MovieSession.all
     if current_user
       @my_cinemas = current_user.cinemas
+      @users = User.all if current_user.admin_role?
     end
   end
   
