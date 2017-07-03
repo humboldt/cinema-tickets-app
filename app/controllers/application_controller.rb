cc3facd329e7712932754bfda8aeb64e8a17ea0a
Oleg Larkin <krabique48@gmail.com>
2017-07-03 18:33:55 +0000

    Add devise redirect after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
   before_action :authenticate_user!
+  before_action :store_current_location, unless: :devise_controller?
   
   
   private
   
   def current_ability
     @current_ability ||= Ability.new(current_user, @cinema)
+  end
+  
+  def store_current_location
+    store_location_for(:user, request.url)
   end
   
 end
