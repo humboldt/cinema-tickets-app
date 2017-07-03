4f917ef113bbb3854cb11337bf3ad95513e81509
Oleg Larkin <krabique48@gmail.com>
2017-07-03 17:59:56 +0000

    Beautify buy form code and duplicate it in the helper

--- app/helpers/movie_sessions_helper.rb
+++ app/helpers/movie_sessions_helper.rb
@@ -20,4 +20,26 @@ module MovieSessionsHelper
     raw all_seats
   end
   
+  def show_buy_form
+    if @movie_session.seats[params[:seat]] == "taken"
+      return (
+        raw '<p class="seat-taken-message">This seat is already taken!</p>')
+    elsif @movie_session.seats[params[:seat]] == "reserved" &&
+      @movie_session.reserved_seats[params[:seat]] != current_user.id
+      return (
+        raw '<p class="seat-reserved-message">This seat is currently reserved!
+        But it might be available shortly...</p>')
+    else
+      return (raw "
+        <p><strong>seat: </strong>#{params[:seat]}</p>
+        <p><strong>price: </strong>
+          #{humanized_money_with_symbol @movie_session.seat_price}
+        </p>
+        #{button_to 'Buy!', 
+          cinema_hall_movie_session_buy_ticket_url(
+          @cinema, @hall, @movie_session,:seat => params[:seat]), 
+          :method => :patch, :class => 'btn btn-primary'}
+      ")
+    end
+  end
 end
