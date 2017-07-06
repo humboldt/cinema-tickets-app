7f643a4e42ea26c466c1315d55d13973fb13506f
Oleg Larkin <krabique48@gmail.com>
2017-07-06 20:40:46 +0000

    Add more efficient relations load (.includes)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   
   def index
     @movies = Movie.all
-    @movie_sessions = MovieSession.all
+    @movie_sessions = MovieSession.includes(:movie, hall: [:cinema]).all
     if current_user
       @my_cinemas = current_user.cinemas
       @users = User.all if current_user.admin_role?
